3be14ee8f6aeeac584512b205271a4aedcf19d5d
Amr Elskhawy <amrelskhawy@gmail.com>
2024-07-19 18:43:48 +0300

    Initialize the Frontend Part

---
+++ ecommerce-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h2 className='text-mainColor'>Hello World</h2>
+      </>
+    )
+  }
+}
+
+export default App
