b91814e20384fac2b3039624e1597cba6f4b5e77
Amr Elskhawy <amrelskhawy@gmail.com>
2024-07-22 00:00:39 +0300

    Complete Button Component Styles

--- ecommerce-frontend/src/components/Button.jsx
+++ ecommerce-frontend/src/components/Button.jsx
@@ -3,8 +3,8 @@ import { Component } from "react";
 class Button extends Component {
   render() {
     return (
-      <button>
-        ADD TO CART
+      <button className="p-6 w-full bg-mainColor flex-center text-white font-bold">
+        {this.props.title || 'Button'}
       </button>
     )
   }
