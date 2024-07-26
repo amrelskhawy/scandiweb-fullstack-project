ef7bd0c91e53b6eb86836e340bf787edaed6d6f1
Amr Elskhawy <amrelskhawy@gmail.com>
2024-07-26 23:09:38 +0300

    Update Button.jsx

--- ecommerce-frontend/src/components/Button.jsx
+++ ecommerce-frontend/src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Component } from "react";
 class Button extends Component {
   render() {
     return (
-      <button className="p-6 w-full bg-mainColor flex-center text-white font-bold">
+      <button className=" w-full bg-mainColor flex-center text-white font-bold">
         {this.props.title || 'Button'}
       </button>
     )
