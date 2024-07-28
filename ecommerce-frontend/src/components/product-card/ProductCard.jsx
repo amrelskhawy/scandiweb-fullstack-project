6ae78312102ae128d9538c82f9ce47a6eb615f45
Amr Elskhawy <amrelskhawy@gmail.com>
2024-07-29 00:13:21 +0300

    Update ProductCard.jsx

--- ecommerce-frontend/src/components/product-card/ProductCard.jsx
+++ ecommerce-frontend/src/components/product-card/ProductCard.jsx
@@ -2,16 +2,20 @@ import { Component } from "react";
 import "./ProductCard.scss"
 class ProductCard extends Component {
 
-
+    state = {
+        product : this.props.product || {}
+    }
 
     render() {
+        const mainImg = this.state.product.gallery.split(',')[0]
+
         return (
             <div className="product-card w-full  group hover:cursor-pointer hover:shadow-lg p-5 transition-all relative ">
-                <img src="Image.png" alt="product image" className="w-full " />
+                <img src={mainImg} alt="product image" className="w-full " />
                 <div className="card-body mt-6">
-                    <p className="text-2xl">Running Short</p>
+                    <p className="text-2xl">{this.props.product.name}</p>
                     <span className="inline-block text-xl font-bold">
-                        $50.00
+                        ${this.props.product.prices}
                     </span>
                 </div>
 
