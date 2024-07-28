54ebf88851442bb1ec498689a7c424a413425b1d
Amr Elskhawy <amrelskhawy@gmail.com>
2024-07-28 22:30:47 +0300

    change a product with another

--- ecommerce-frontend/src/pages/ProductDetails/ProductDetails.jsx
+++ ecommerce-frontend/src/pages/ProductDetails/ProductDetails.jsx
@@ -10,57 +10,81 @@ class ProductDetails extends Component {
 
   state = {
     product: {
-      "id": "jacket-canada-goosee",
-      "name": "Jacket",
-      "inStock": true,
+      "id": "xbox-series-s",
+      "name": "Xbox Series S 512GB",
+      "inStock": false,
       "gallery": [
-        "https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016105/product-image/2409L_61.jpg",
-        "https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016107/product-image/2409L_61_a.jpg",
-        "https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016108/product-image/2409L_61_b.jpg",
-        "https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016109/product-image/2409L_61_c.jpg",
-        "https://images.canadagoose.com/image/upload/w_480,c_scale,f_auto,q_auto:best/v1576016110/product-image/2409L_61_d.jpg",
-        "https://images.canadagoose.com/image/upload/w_1333,c_scale,f_auto,q_auto:best/v1634058169/product-image/2409L_61_o.png",
-        "https://images.canadagoose.com/image/upload/w_1333,c_scale,f_auto,q_auto:best/v1634058159/product-image/2409L_61_p.png"
+        "https://images-na.ssl-images-amazon.com/images/I/71vPCX0bS-L._SL1500_.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/71q7JTbRTpL._SL1500_.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/71iQ4HGHtsL._SL1500_.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/61IYrCrBzxL._SL1500_.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/61RnXmpAmIL._SL1500_.jpg"
       ],
-      "description": "<p>Awesome winter jacket</p>",
-      "category": "clothes",
+      "description": "\n<div>\n    <ul>\n        <li><span>Hardware-beschleunigtes Raytracing macht dein Spiel noch realistischer</span></li>\n        <li><span>Spiele Games mit bis zu 120 Bilder pro Sekunde</span></li>\n        <li><span>Minimiere Ladezeiten mit einer speziell entwickelten 512GB NVMe SSD und wechsle mit Quick Resume nahtlos zwischen mehreren Spielen.</span></li>\n        <li><span>Xbox Smart Delivery stellt sicher, dass du die beste Version deines Spiels spielst, egal, auf welcher Konsole du spielst</span></li>\n        <li><span>Spiele deine Xbox One-Spiele auf deiner Xbox Series S weiter. Deine Fortschritte, Erfolge und Freundesliste werden automatisch auf das neue System übertragen.</span></li>\n        <li><span>Erwecke deine Spiele und Filme mit innovativem 3D Raumklang zum Leben</span></li>\n        <li><span>Der brandneue Xbox Wireless Controller zeichnet sich durch höchste Präzision, eine neue Share-Taste und verbesserte Ergonomie aus</span></li>\n        <li><span>Ultra-niedrige Latenz verbessert die Reaktionszeit von Controller zum Fernseher</span></li>\n        <li><span>Verwende dein Xbox One-Gaming-Zubehör -einschließlich Controller, Headsets und mehr</span></li>\n        <li><span>Erweitere deinen Speicher mit der Seagate 1 TB-Erweiterungskarte für Xbox Series X (separat erhältlich) und streame 4K-Videos von Disney+, Netflix, Amazon, Microsoft Movies &amp; TV und mehr</span></li>\n    </ul>\n</div>",
+      "category": "tech",
       "attributes": [
         {
-          "id": "Size",
+          "id": "Color",
           "items": [
             {
-              "displayValue": "Small",
-              "value": "S",
-              "id": "Small",
+              "displayValue": "Green",
+              "value": "#44FF03",
+              "id": "Green",
               "__typename": "Attribute"
             },
             {
-              "displayValue": "Medium",
-              "value": "M",
-              "id": "Medium",
+              "displayValue": "Cyan",
+              "value": "#03FFF7",
+              "id": "Cyan",
               "__typename": "Attribute"
             },
             {
-              "displayValue": "Large",
-              "value": "L",
-              "id": "Large",
+              "displayValue": "Blue",
+              "value": "#030BFF",
+              "id": "Blue",
               "__typename": "Attribute"
             },
             {
-              "displayValue": "Extra Large",
-              "value": "XL",
-              "id": "Extra Large",
+              "displayValue": "Black",
+              "value": "#000000",
+              "id": "Black",
+              "__typename": "Attribute"
+            },
+            {
+              "displayValue": "White",
+              "value": "#FFFFFF",
+              "id": "White",
               "__typename": "Attribute"
             }
           ],
-          "name": "Size",
+          "name": "Color",
+          "type": "swatch",
+          "__typename": "AttributeSet"
+        },
+        {
+          "id": "Capacity",
+          "items": [
+            {
+              "displayValue": "512G",
+              "value": "512G",
+              "id": "512G",
+              "__typename": "Attribute"
+            },
+            {
+              "displayValue": "1T",
+              "value": "1T",
+              "id": "1T",
+              "__typename": "Attribute"
+            }
+          ],
+          "name": "Capacity",
           "type": "text",
           "__typename": "AttributeSet"
         }
       ],
       "prices": [
         {
-          "amount": 518.47,
+          "amount": 333.99,
           "currency": {
             "label": "USD",
             "symbol": "$",
@@ -69,7 +93,7 @@ class ProductDetails extends Component {
           "__typename": "Price"
         }
       ],
-      "brand": "Canada Goose",
+      "brand": "Microsoft",
       "__typename": "Product"
     }
   }
